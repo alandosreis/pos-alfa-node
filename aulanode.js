6d02116ed03b5d791ce4cc6cb285594bd9eb6324
alandosreis <faculdadealfa@Mac-10.local>
2016-08-06 13:55:43 -0300

    commit master

--- aulanode.js
+++ aulanode.js
@@ -1,27 +1,67 @@
-
-
 const http = require("http")
 
 http.createServer((request, response) => {
-  response.writeHead(200, {"Content-Type": "aplication/json;charset=utf-8"})
+  response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"})
 
   const url = request.url
   console.log('url', url)
 
-  switch(url){
-  	case '/listar':
-  	const listagem = [
-  	{name:"Suissa", age: 31},
-  	{name:"João", age: 24},
-  	{name:"José", age: 23},
-  	{name:"Maria", age: 22}
-  	]
-  }
-  	
+require('./config')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const _schema = {
+  name:  String,
+  atack: Number,
+  defense: Number
+}
+const pokemonSchema = new Schema(_schema);
+const PokemonModel = mongoose.model('Pokemon', pokemonSchema);
 
-  
+ switch (url) {
+ 	case '/criar':
+  	const Suissamon = new PokemonModel({ name: 'Suissamon' });
+    // Suissamon.save(function (err, data) {
+    //   if (err) return console.log('ERRO: ', err);
+    //   return console.log('Inseriu:', data);
+    // });
+    // ou
+    PokemonModel.create({ name: 'Suissamon' }, function (err, data) {
+      if (err) return console.log('ERRO: ', err);
+      return console.log('Inseriu:', data);
+    }); 
+  	break;
+  case '/listar':
+  	const query ={}
 
-  response.write('Hello Wordsld')
+    PokemonModel.find(query, function (err, data) {
+      if (err) return console.log('ERRO: ', err);
+      return console.log('Buscou:', data);
+    })
+  	break;
+  case '/alterar':
+  	/*const query = { name: /suissamon/i}
+    const mod = {attack: 9001}
+    const options = {
+      multi: true,
+      upsert: true
+    }*/
+
+    PokemonModel.update(query, mod, (err, data) => {
+      if (err) return console.log('ERRO: ', err)
+      return console.log('Buscou:', data)
+})
+  	break;
+  	case '/deletar':
+  	response.write(JSON.stringify(listagem))
+  	break;
+  	default:
+  	response.end('Rota não encontrada')
+  	break;
+ }
+
+
+
+  response.write('Hello World')
 
   response.end()
 })
